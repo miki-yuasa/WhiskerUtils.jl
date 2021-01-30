2d59223d0012feda37acdb8db4c350486eca0b08
Mikihisa Yuasa <mikihisa.yuasa@wisc.edu>
2021-01-29 22:24:03 -0600

    edits

---
+++ playGround.jl
@@ -0,0 +1,67 @@
+##
+using Plots
+gr()
+x = range(-2, stop=2, length=100)
+y = range(sqrt(2), stop=2, length=100)
+f(x,y) = x * y - x - y + 1
+
+plot(x,y,f,st=:surface,camera=(-30, 30))
+
+##
+plot(rand(3))
+plot!(rand(5),bordercolor=:white,        gridcolor=:white
+)
+
+##
+using3D()
+x = -1.5:0.1:1.5
+y = -1:0.1:1
+z(x,y) = 3.434
+
+plt = surface(x, y, z, fillcolor=:blue, α=0.8, grid=false, linecolor=:black, edgecolors="k")
+
+
+
+##
+using PyPlot
+
+thetas = range(0, stop=pi,   length=50)
+phis   = range(0, stop=pi / 2, length=50)
+
+X(r,theta,phi) = r * sin(theta) * sin(phi)
+Y(r,theta,phi) = r * sin(theta) * cos(phi)
+Z(r,theta,phi) = r * cos(theta)
+
+xs = [X(1, theta, phi) for theta in thetas, phi in phis] 
+ys = [Y(1, theta, phi) for theta in thetas, phi in phis]
+zs = [Z(1, theta, phi) for theta in thetas, phi in phis]
+
+plot_surface(xs, ys, zs, alpha=1)
+
+gcf()
+
+##
+using PyCall
+plot = pyimport("matplotlib.pyplot")
+mpl3 = pyimport("mpl_toolkits.mplot3d")
+
+using PyPlot
+
+x = -1.5:0.1:1.5
+y = -1:0.1:1
+z(x,y) = 3.434
+
+plot_surface(x,y,z)
+
+##
+
+using PyPlot
+
+x = -1.5:0.1:1.5
+y = -1:0.1:1
+f(x,y) = 3.434
+
+z = f.(x', y)
+fig = surf(x, y, z)
+
+
---
+++ plotSurface.jl
@@ -0,0 +1,24 @@
+##
+function plot_surface(x, y, z, M, nM; camera::NTuple{2,Float64}=(-30, 30), plotSlice::Bool=false, saveFig::Bool=false)
+
+    surface(z, x, y)
+    surface!(z, x, -y, 
+        camera=camera, 
+        xlim=(0, nM * M), 
+        palette=:haline, 
+        bordercolor=:white,
+        gridcolor=:white
+        )
+
+    if plotSlice
+        X = -1.5:0.1:1.5
+        Y = -1:0:1
+        Z(X, Y) = 1
+        surface(Z, X, Y, 
+        fillcolor=:blue,
+        alpha=0.8,
+        gridcolor=:none)
+    end
+
+
+end
